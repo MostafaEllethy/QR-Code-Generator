19ed523ddadf7f9fa55a62d4317b9e6d87cef8ac
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2020-03-28 19:30:57 +0200

    Init

---
+++ Site/src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="appHeader">
+      <q-toolbar>
+        <q-toolbar-title class="text-weight-medium">
+         QR Code Generator
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .appHeader {
+    background: linear-gradient(145deg,#0f477e 11%,#1976d2 75%);
+  }
+</style>
---
+++ Site/src/pages/Index2.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <router-link to="/">Go to Index1</router-link>
+    <textarea rows="10" style="width: 500px" v-model="string">
+</textarea>
+    <canvas id="canvas"></canvas>
+  </q-page>
+</template>
+
+<script>
+  import QRCode from 'qrcode';
+
+  export default {
+    name: 'PageIndex',
+    data() {
+      return {
+        string: ""
+      }
+    }, watch: {
+      string(val) {
+        if (val != null) {
+          console.log(val);
+          var canvas = document.getElementById('canvas');
+          QRCode.toCanvas(canvas, val, { width: 250 });
+        }
+      }
+    }
+  }
+</script>

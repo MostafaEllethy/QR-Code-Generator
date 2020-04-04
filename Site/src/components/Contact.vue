c1d05de91a955d07e03d4485965f80bff473d9ba
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2020-04-04 17:23:39 +0200

    u

--- Site/src/components/Contact.vue
+++ Site/src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <q-dialog v-model="contactUs">
+    <q-dialog v-model="contactUs" :persistent="sending">
       <q-card>
         <form @submit.prevent="submitForm">
           <q-toolbar>
@@ -8,7 +8,7 @@
             <q-btn flat round icon="close" v-close-popup />
           </q-toolbar>
           <q-separator />
-          <q-card-section style="max-height: 75vh;min-width:325px;min-height: 80px;" class="scroll">
+          <q-card-section style="max-height: 60vh;min-width:325px;min-height: 80px;" class="scroll">
             <div class="row q-col-gutter-sm">
               <template v-if="sent">
                 <div class="col-12 text-h5 text-blue-10 text-center">
@@ -17,13 +17,13 @@
               </template>
               <template v-else>
                 <div class="col-12">
-                  <q-input filled v-model="contact.name" label="Name" required :readonly="loading" />
+                  <q-input filled v-model="contact.name" label="Name" required :readonly="sending" />
                 </div>
                 <div class="col-12">
-                  <q-input filled v-model="contact.email" label="Email" :readonly="loading" />
+                  <q-input filled v-model="contact.email" label="Email" :readonly="sending" />
                 </div>
                 <div class="col-12">
-                  <q-input filled v-model="contact.message" label="Message" type="textarea" rows="5" required :readonly="loading" />
+                  <q-input filled v-model="contact.message" label="Message" type="textarea" rows="5" required :readonly="sending" />
                 </div>
               </template>
             </div>
@@ -31,7 +31,7 @@
           <template v-if="!sent">
             <q-separator />
             <q-card-actions align="right">
-              <q-btn type="submit" color="positive" icon-right="send" flat label="Send" :loading="loading" />
+              <q-btn type="submit" color="positive" icon-right="send" flat label="Send" :loading="sending" />
             </q-card-actions>
           </template>
         </form>
@@ -65,7 +65,7 @@
       return {
         contact: {}
         , contactUs: false
-        , loading: false
+        , sending: false
         , sent: false
       }
     }
@@ -80,9 +80,11 @@
         this.contactUs = true;
       }
       , submitForm() {
-        this.loading = true;
-        this.loading = false;
-        this.sent = true;
+        this.sending = true;
+        module.$axios.post(window.apiUrl + '/api/Contacts', this.contact).then(() => {
+          this.sending = false;
+          this.sent = true;
+        })
       }
     }
   }

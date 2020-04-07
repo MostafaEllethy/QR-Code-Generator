d99fcd658920b73317031f8d6af05e180cab3eed
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2020-04-07 08:47:53 +0200

    u

--- Site/src/layouts/QR.vue
+++ Site/src/layouts/QR.vue
@@ -9,83 +9,82 @@
           <QR ref="QR" :screenSize="screenSize"></QR>
         </div>
       </div>
-      <q-separator spaced />
-      <div class="row" id="FAQ">
-        <div class="col-12">
-          <h2 style="color: #5c5e70;" class="text-h5 text-center text-weight-bolder"><img src="~assets/faq.png" width="65" style="vertical-align:middle;" v-if="$q.screen.name !== 'xs'"/> Frequently Asked Questions</h2>
-          <div class="row q-col-gutter-sm">
-            <div class="col-xs-12 col-sm-6">
-              <q-list>
-                <q-expansion-item label="What is QR Code?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-                <q-expansion-item label="How QR Code Generator works?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-                <q-expansion-item label="My QR code is not working, what can I do?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-              </q-list>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-              <q-list v-bind:class="{'q-mt-md': $q.screen.name === 'xs'}">
-                <q-expansion-item label="What is QR Code?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-                <q-expansion-item label="How QR Code Generator works?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-                <q-expansion-item label="My QR code is not working, what can I do?" popup>
-                  <q-separator />
-                  <q-card>
-                    <q-card-section>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                      commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                      eveniet doloribus ullam aliquid.
-                    </q-card-section>
-                  </q-card>
-                </q-expansion-item>
-              </q-list>
-            </div>
+    </q-card-section>
+    <div class="row q-px-md q-pb-lg bg-grey-2" id="FAQ">
+      <div class="col-12">
+        <h2 style="color: #5c5e70;" class="text-h5 text-center text-weight-bolder"><img src="~assets/faq.png" width="65" style="vertical-align:middle;" v-if="$q.screen.name !== 'xs'" /> Frequently Asked Questions</h2>
+        <div class="row q-col-gutter-sm">
+          <div class="col-xs-12 col-sm-6">
+            <q-list>
+              <q-expansion-item label="What is QR Code?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item label="How QR Code Generator works?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item label="My QR code is not working, what can I do?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+            </q-list>
+          </div>
+          <div class="col-xs-12 col-sm-6">
+            <q-list v-bind:class="{'q-mt-md': $q.screen.name === 'xs'}">
+              <q-expansion-item label="What is QR Code?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item label="How QR Code Generator works?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+              <q-expansion-item label="My QR code is not working, what can I do?" popup>
+                <q-separator />
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+            </q-list>
           </div>
         </div>
       </div>
-    </q-card-section>
+    </div>
   </div>
 </template>
 
